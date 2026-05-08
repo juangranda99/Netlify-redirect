184502ec37cde885251b1d03d854bdb74e30af9e
Juan <granda@Juans-MacBook-Air.local>
2026-05-07 22:46:29 -0300

    mensaje del cambio

--- functions/redirect.js
+++ functions/redirect.js
@@ -28,10 +28,10 @@ exports.handler = async (event) => {
 
     // Objeto para las imágenes con códigos cortos
     const images = {
-      i1: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/7085%20-%20Comcast%2FFireShot%20Capture%20212%20-%20%20-%20%5B%5D.jpg?alt=media&token=69d80d6c-d775-4951-8161-9a77425c7833', // 500 1251
-      i2: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/7085%20-%20Comcast%2FScreenshot%202025-10-03%20at%208.39.40%E2%80%AFPM.jpg?alt=media&token=cc71c9ac-e280-4784-b28c-d39a9e38a1aa', // 500 41
-      i3: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/8951%20-%20Comcast%2FScreenshot%202026-03-24%20at%209.34.30%E2%80%AFPM.jpg?alt=media&token=a62bcebd-7a17-4bb6-96ec-7e2683dcd985', // 500 623
-      i4: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/8951%20-%20Comcast%2FScreenshot%202025-10-07%20at%209.04.37%E2%80%AFPM.jpg?alt=media&token=0177df5b-701d-48a5-8d4f-290e36e639bc', // 500 67
+      i1: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/9035%20-%20CPC%2FScreenshot%202026-03-04%20at%2011.09.52%E2%80%AFPM.jpg?alt=media&token=d1afb9f6-dade-4882-a220-40a98074db9c', // 500 611
+      i2: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/9035%20-%20CPC%2FScreenshot%202025-09-25%20at%2000.24.27%20(1).jpg?alt=media&token=dca7b9d3-4c14-4d9c-b4a3-206f10902890', // 500 60
+      i3: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/4455%20-%20Comcast%2FScreenshot%202026-05-07%20at%2010.45.25%E2%80%AFPM.jpg?alt=media&token=17222296-2105-4b5c-91b3-ca239fc2803e', // 500 541
+      i4: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/4455%20-%20Comcast%2FScreenshot%202026-01-08%20at%202.46.56%E2%80%AFPM.jpg?alt=media&token=18123b18-53f5-40e2-a8e1-a54b26eb5764', // 500 66
       i5: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/7909%20-%20Comcast%2FFireShot%20Capture%20218%20-%20%20-%20%5B%5D.jpg?alt=media&token=421425f1-1f29-4be4-91a6-2d5e6a9a666c', // 500 1243
       i6: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/7909%20-%20Comcast%2FScreenshot%202026-02-06%20at%208.46.51%E2%80%AFPM.jpg?alt=media&token=810bce18-08e2-4b6c-839d-e9804d81ab9f', // 500 83
       // Podes agregar más imágenes aquí con códigos cortos
@@ -461,10 +461,10 @@ exports.handler = async (event) => {
       p: 'https://www.zopistreo.com/BD7N3BM8/GK847MZ/?__efq=EHofFgjJ9fhvrKq1pOxVmZlixbkYJUQwhwev-h_gDV9uH5d7K4c3-O2Utii_B8VcKMz0vFR0XSQ',
       u: 'https://www.zopistreo.com/integration/unsub1/?_redir=CiUAgbOb1fP7E0Qk_zQFqfBtDXumVw5gUnYBP6hmx4rY6VNaGcn7EnEA1NLZaWMx9Fe7yyKIpxnubkrqw5MWDCXqHJnaE8RbdzZfoLxlKUg-QYEhT51zFiMDCM7K7NhYTy4-Ows6yTyb5DT70h1Wtrsj5N20Z6he0nUkn6kEHmNJpQVljEsaRVx4IgH_IEbbNfsmPrBRKA7UCA'
     },
-    // 9035 CPC - Quick Auto Coverage - Ask for cap $2.550  daniel+162012@aguara.com.ar
+    // 9035 CPC - Quick Auto Coverage - Ask for cap $2.550  daniel+162111@aguara.com.ar
     qik: {
-      r: 'https://www.zalkkweet.com/BCN956MP/K13SQGJ/?__efq=CaNag-E1snVXb2r39UTFOkxlBQvkq866gSxgfULNnOOPsvltxZ6uA6qULiuhL-AE',
-      u: 'https://www.zalkkweet.com/integration/unsub1/?_redir=CiUAgbOb1d0ScNdSZ5duz9-kRRHxpqzY6DlRsRa-X4pRwXvjSIvAEnUA1NLZaWgn4eHHFh-ZaC36OVv5rrjxU259y9wHILu6CI6e80_6_yKnMe0X98awujraGF2PDOBRBtJoG19FKXZ-cYbTn1-avYbE4wd5znEVoKigH7Nt9nX-q91JkjQr8tF1pNgJTeeiZF7s-rXnjc1ym6TtWbg'
+      r: 'https://www.zalkkweet.com/BCSQZXDD/K13SQGJ/?__efq=CaNag-E1snVXb2r39UTFOkxlBQvkq866gSxgfULNnOOPsvltxZ6uA3PxpQy4fYlj',
+      u: 'https://www.zalkkweet.com/integration/unsub1/?_redir=CiUAgbOb1f2YrJ4T1XOU1XFUwz37TqZ0aymJIDveKdRO_vq5DYanEnQAS6SNtP_Hc39scFCWZrRxFsdH3vp4Lx3Ric6xoz8X9V8avfhDTXSPsTG2GMdCeDqyA7Xcx2gvZjThNufzYJj6ykqDVnZAWuX7TIF1WI9wqmcFx9xIc3rsBOyq8v0qHZatyNTPokO--TNIsTVRJU_5g-uEDw'
     },
     // 8970 CPC - 2025 Personal Loans - Ask for Cap $1.275 
     noa: {
