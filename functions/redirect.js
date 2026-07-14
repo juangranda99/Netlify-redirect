37aa2886be56cfd51129c9beeaf5567d3447ca5b
Juan <granda@Juans-MacBook-Air.local>
2026-07-14 14:44:55 -0300

    mensaje del cambio

--- functions/redirect.js
+++ functions/redirect.js
@@ -66,8 +66,8 @@ exports.handler = async (event) => {
 
     // Objeto para las imágenes con códigos cortos
     const images = {
-      i1: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/8787%20-%20Comcast%2FFireShot%20Capture%20291%20-%20Vehicle%20Protection%20USA%20-%20%5B%5D.jpg?alt=media&token=ce937cf9-637e-40ba-a155-cb8fe26ea3b7', // 500 1035
-      i2: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/8787%20-%20Comcast%2FScreenshot%202026-06-17%20at%2012.22.17%E2%80%AFAM.jpg?alt=media&token=4cb1786c-a585-4b6f-8805-72229a452a15', // 500 134
+      i1: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/8238%20-%20Comcast%2FFireShot%20Capture%20015%20-%20Vanguard%20Home%20Warranty%20-%20Find%20the%20best%20home%20warranty%20plans%20-%20%5B%5D.jpg?alt=media&token=5015ee1c-5c64-41ff-bd2b-2e56e3df809a', // 500 946
+      i2: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/8238%20-%20Comcast%2FScreenshot%202025-12-15%20at%206.16.04%E2%80%AFPM.jpg?alt=media&token=23a6e006-b49e-4482-afb1-a2d1de36585b', // 500 142
       i3: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/7155%20-%20Comcast%2FScreenshot%202026-01-19%20at%202.55.51%E2%80%AFPM.jpg?alt=media&token=eefab2cd-e05d-4ea7-9412-ff9bb0aee4be', // 500 68
       i4: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/9574%20-%20Comcast%2FScreenshot%202026-06-29%20at%2011.44.49%E2%80%AFPM.jpg?alt=media&token=e35c30da-a18a-4a7f-8930-498a8e0b1830', // 500 54
       i5: 'https://firebasestorage.googleapis.com/v0/b/play-c33aa.firebasestorage.app/o/7085%20-%20Comcast%2FScreenshot%202025-10-03%20at%208.39.40%E2%80%AFPM.jpg?alt=media&token=cc71c9ac-e280-4784-b28c-d39a9e38a1aa', // 500 41
